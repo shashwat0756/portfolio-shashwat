c9b324110632e7f70f8de17a713a8a4ba3d1bc64
shashwat0756 <shashwatsandilya514@gmail.com>
2023-06-04 23:51:25 +0530

    Adding new content to my portfolio

--- port.py
+++ port.py
@@ -3,7 +3,8 @@ st.set_page_config(layout="wide")
 col1,col2 = st.columns(2)
 
 with col1:
-    st.image("images/img.jpg")
+    st.image("images/img.jpg",width=500)
+
 
 with col2:
     st.title("Shashwat Shandilya")
@@ -11,3 +12,8 @@ with col2:
     Hi I am Shashwat Shandilya a Python Programmer 
     """
     st.info(content)
+
+content2 = """
+Below you can find some of the Apps I have built using Python. Feel free to connect!
+"""
+st.write(content2)
